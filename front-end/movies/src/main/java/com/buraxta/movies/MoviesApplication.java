f73474ed3a3e083b0b6479685c5ff2138622d4d8
buraxta <burakgulluler@gmail.com>
2023-07-16 23:52:07 +0300

    front-end section

--- front-end/movies/src/main/java/com/buraxta/movies/MoviesApplication.java
+++ front-end/movies/src/main/java/com/buraxta/movies/MoviesApplication.java
@@ -1,7 +1,10 @@
 package com.buraxta.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MoviesApplication {
@@ -9,6 +12,7 @@ public class MoviesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
+
 
 
 }
