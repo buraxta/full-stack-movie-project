4f969ad1a001191acf52176e0d0f872c67c00263
buraxta <burakgulluler@gmail.com>
2023-07-17 14:24:21 +0300

    backend section

---
+++ back-end/movie-client/movie-gold-v1/src/App.js
@@ -0,0 +1,65 @@
+import "./App.css";
+import api from "./api/axiosConfig";
+import { useState, useEffect } from "react";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import Header from "./components/header/Header";
+import Trailer from "./components/trailer/Trailer";
+import Reviews from "./components/reviews/Reviews";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState();
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get("/api/v1/movies");
+      console.log(response.data);
+      setMovies(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  const getMovieData = async (movieId) => {
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+      const singleMovie = response.data;
+      setMovie(singleMovie);
+      setReviews(singleMovie.reviewIds);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies} />} />
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer />} />
+          <Route
+            path="/Reviews/:movieId"
+            element={
+              <Reviews
+                getMovieData={getMovieData}
+                reviews={reviews}
+                setReviews={setReviews}
+                movie={movie}
+              />
+            }
+          />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
